54278888c273af23471ad1d456bc0c64d83e1a54
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2021-05-11 16:10:08 +0200

    remove debug prints

--- run_unit_tests.py
+++ run_unit_tests.py
@@ -2,12 +2,10 @@
 import unittest
 import sys
 import os
-print("a")
+
 os.environ["MPLBACKEND"] = "agg"
-print("a")
 loader = unittest.TestLoader()
 unit_tests = loader.discover('./tests/unit_tests', top_level_dir='.')
-print("a")
 unit_test_runner = unittest.runner.TextTestRunner()
 unit_test_results = unit_test_runner.run(unit_tests)
 
