86717d29e3a2c2b6dc44d47f2505d2dd120901d3
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2021-06-08 17:20:50 +0200

    update import

--- tests/unit_tests/modem/test_ldpc_encoder.py
+++ tests/unit_tests/modem/test_ldpc_encoder.py
@@ -8,7 +8,6 @@ from scipy.io import loadmat
 
 from modem.coding.ldpc_encoder import LdpcEncoder
 from parameters_parser.parameters_ldpc_encoder import ParametersLdpcEncoder
-import modem.coding.ldpc_binding.ldpc_binding as ldpc_binding
 
 
 class TestLdpcEncoder(unittest.TestCase):
