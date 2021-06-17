c3cb24d83939b1664712334a89a233f9fc9f72c9
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2021-06-17 10:20:32 +0200

    add (buggy) implementation of encode function

--- modem/coding/ldpc_binding/ldpc_binding.cpp
+++ modem/coding/ldpc_binding/ldpc_binding.cpp
@@ -5,17 +5,48 @@
 
 #include <vector>
 #include <iostream>
+#include <Eigen/LU>
 
 namespace py = pybind11;
+using Eigen::Dynamic;
+typedef Eigen::Matrix<int, 1, Eigen::Dynamic> RowVector;
 
-std::vector<py::array_t<int>> encode(
-    const std::vector<py::array_t<int>> &data_bits,
-    const py::array_t<int> &G, //check if not 2d
-    const py::array_t<int> &Z,
+std::vector<RowVector> encode(
+    const std::vector<RowVector> &data_bits,
+    const Eigen::Matrix<int, Dynamic, Dynamic> &G, //check if not 2d
+    const int Z,
+    const int num_info_bits,
     const int encoded_bits_n,
+    const int data_bits_k,
+    const int code_blocks,
     const int bits_in_frame)
 {
-    return data_bits;
+    int no_bits = 0;
+    std::vector<RowVector> encoded_words;
+    for (auto block : data_bits)
+    {
+        if (block.size() % data_bits_k != 0)
+            exit(0);
+
+        for (int code_block_idx = 0; code_block_idx < code_blocks; code_block_idx++)
+        {
+            RowVector code_word = block(0, Eigen::seq(0, num_info_bits - 1)) * G;
+            code_word = code_word.unaryExpr([](auto x)
+                                            { return x % 2; });
+            code_word = code_word(0, Eigen::seq(2 * Z, Eigen::last));
+            block = block(0, Eigen::seq(num_info_bits, block.cols() - 1));
+            no_bits += encoded_bits_n;
+        }
+
+        if ((bits_in_frame - no_bits) > 0)
+        {
+            RowVector fillup_data = (Eigen::MatrixXi::Random(1, bits_in_frame - no_bits).array() + 1) / 2;
+
+            std::cout << "adding " << fillup_data << std::endl;
+            encoded_words.push_back(fillup_data);
+        }
+    }
+    return encoded_words;
 }
 
 std::vector<py::array_t<int>> decode(
