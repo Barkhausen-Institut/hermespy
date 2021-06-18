96f22cbb496adcc1c67f0ed652fd8ae324cee3e1
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2021-06-18 10:26:17 +0200

    add further tests for python binding

--- tests/unit_tests/modem/test_ldpc_encoder.py
+++ tests/unit_tests/modem/test_ldpc_encoder.py
@@ -179,6 +179,29 @@ class TestLdpcEncoder(unittest.TestCase):
 
         np.testing.assert_array_almost_equal(decoded_words, expected_decoded_words)
 
+    def test_properDecoding_multipleBlocks_pythonBinding(self) -> None:
+        params = ParametersLdpcEncoder()
+        params.code_ratio = 2 / 3
+        params.block_size = 256
+        params.code_rate_fraction = Fraction(2, 3)
+        params.custom_ldpc_codes = ""
+        params.use_binding = True
+
+        ldpc_results_mat = loadmat(os.path.join(
+                self.encoderTestResultsDir, 'test_data_decoder_256_2_3.mat'
+            ), squeeze_me=True)
+        params.no_iterations = ldpc_results_mat['LDPC']['iterations'].item()
+
+        bits_in_frame = len(ldpc_results_mat['llrs'][0])
+
+        encoder = LdpcEncoder(params, bits_in_frame)
+
+        expected_decoded_words = ldpc_results_mat['est_bit_words']
+        llrs = -ldpc_results_mat['llrs']
+        decoded_words = encoder.decode(llrs)
+
+        np.testing.assert_array_almost_equal(decoded_words, expected_decoded_words)
+
     def test_noDataBitsLongerThanCodeBlock(self) -> None:
         data_word_too_long = np.random.randint(2, size=self.encoder.data_bits_k + 3)
 
@@ -207,10 +230,39 @@ class TestLdpcEncoder(unittest.TestCase):
 
         self.assertEqual(len(decoded_bits[0]), self.no_code_blocks * self.kActual)
 
+    def test_decoding_checkIfFillUpBitsAreDiscarded_pythonBinding(self) -> None:
+        no_bits_filled_up = 10
+        self.bits_in_frame += no_bits_filled_up
+        self.params.use_binding = True
+
+        encoder = LdpcEncoder(self.params, self.bits_in_frame)
+        encoded_bits = []
+
+        for block_idx in range(self.no_code_blocks):
+            encoded_bits.append(np.random.randint(2, size=self.nActual))
+
+        encoded_bits.append(np.random.randint(2, size=no_bits_filled_up))
+        decoded_bits = encoder.decode([flatten_blocks(encoded_bits)])
+
+        self.assertEqual(len(decoded_bits[0]), self.no_code_blocks * self.kActual)
+
     def test_ifCodeBlocksDoNotFillUpFrame(self) -> None:
         no_bits_frame_too_long = 10
         self.bits_in_frame += no_bits_frame_too_long
         no_data_bits = self.no_code_blocks * self.kActual
+
+        encoder = LdpcEncoder(self.params, self.bits_in_frame)
+
+        data_bits_frame = np.random.randint(2, size=no_data_bits)
+        encoded_frame = encoder.encode([data_bits_frame])
+
+        self.assertEqual(len(encoded_frame[-1]), no_bits_frame_too_long)
+
+    def test_ifCodeBlocksDoNotFillUpFrame_pythonBinding(self) -> None:
+        no_bits_frame_too_long = 10
+        self.bits_in_frame += no_bits_frame_too_long
+        no_data_bits = self.no_code_blocks * self.kActual
+        self.params.use_binding = True
 
         encoder = LdpcEncoder(self.params, self.bits_in_frame)
 
