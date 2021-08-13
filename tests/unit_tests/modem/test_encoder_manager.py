4baa81d45135b2655dbacb39f0cab8dc0d8fd513
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2021-08-13 11:02:24 +0200

    k of next encoder may also be larger than of current encoder

--- tests/unit_tests/modem/test_encoder_manager.py
+++ tests/unit_tests/modem/test_encoder_manager.py
@@ -20,22 +20,26 @@ class StubEncoder(Encoder):
 
 
 class TestEncoderManager(unittest.TestCase):
+    def setUp(self) -> None:
+        self.params_encoder1 = ParametersEncoder()
+        self.params_encoder1.data_bits_k = 3
+        self.params_encoder1.encoded_bits_n = 5
+
+        self.params_encoder2 = ParametersEncoder()
+        self.params_encoder2.data_bits_k = 6
+        self.params_encoder2.encoded_bits_n = 7
+
+        self.encoder1 = StubEncoder(self.params_encoder1, 100)
+        self.encoder2 = StubEncoder(self.params_encoder2, 100)
+
     def test_correct_order_of_encoders(self) -> None:
-        params_encoder1 = ParametersEncoder()
-        params_encoder1.data_bits_k = 3
-        params_encoder1.encoded_bits_n = 5
-
-        params_encoder2 = ParametersEncoder()
-        params_encoder2.data_bits_k = 5
-        params_encoder2.encoded_bits_n = 7
-
-        encoder1 = StubEncoder(params_encoder1, 100)
-        encoder2 = StubEncoder(params_encoder2, 100)
-
         encoder_manager = EncoderManager()
-        self.assertTrue(encoder_manager.add_encoder(encoder1))
-        self.assertTrue(encoder_manager.add_encoder(encoder2))
+        self.assertTrue(encoder_manager.add_encoder(self.encoder1))
+        self.assertTrue(encoder_manager.add_encoder(self.encoder2))
         self.assertEqual(len(encoder_manager.encoders), 2)
 
-
-        
+    def test_ordering_of_encoders(self) -> None:
+        encoder_manager = EncoderManager()
+        self.assertTrue(encoder_manager.add_encoder(self.encoder2))
+        self.assertTrue(encoder_manager.add_encoder(self.encoder1))
+        self.assertEqual(len(encoder_manager.encoders), 2)
