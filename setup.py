656819ef01592776e3458b56827f50dd30ee3944
Jan Adler <jan.adler@barkhauseninstitut.org>
2021-09-02 15:18:23 +0200

    Configured build scripts for scikit-build/pybind11

--- setup.py
+++ setup.py
@@ -1,27 +1,15 @@
-from setuptools import setup, Extension
-from pybind11.setup_helpers import Pybind11Extension, build_ext
-from pybind11 import get_cmake_dir
-
-import sys
-import os
-
-
-ext_modules = [
-    Pybind11Extension("ldpc_binding",
-        [os.path.join("modem", "coding", "ldpc_binding", "ldpc_binding.cpp")],
-        include_dirs = ['3rdparty']
-    ),
-]
+from skbuild import setup
+from setuptools import find_packages
 
 setup(
-    name="ldpc_binding",
+    name="hermespy",
     author="Tobias Kronauer",
     author_email="tobias.kronauer@bi-dd.de",
-    description="A test project using pybind11",
+    description="",
     long_description="",
-    ext_modules=ext_modules,
-    # Currently, build_ext only provides an optional "highest supported C++
-    # level" feature, but in the future it may provide more features.
-    cmdclass={"build_ext": build_ext},
-    zip_safe=False,
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    extras_require={"test": ["pytest"]},
+    zip_safe=False
 )
