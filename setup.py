6efe25f72737696fa2080030055e102d63852374
jan.adler <jan.adler@barkhauseninstitut.org>
2021-09-02 17:16:26 +0200

    Added package indicator

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="hermespy",
+    version="0.1.0",
     author="Tobias Kronauer",
     author_email="tobias.kronauer@bi-dd.de",
     description="",
@@ -10,5 +11,6 @@ setup(
     packages=find_packages('.', exclude=("tests",)),
     include_package_data=True,
     extras_require={"test": ["pytest"]},
+    cmake_install_dir=".",
     zip_safe=False
 )
