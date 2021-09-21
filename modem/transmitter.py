fe130796753d055ce478adc45696aa6f4235927a
jan.adler <jan.adler@barkhauseninstitut.org>
2021-09-21 13:31:55 +0200

    BitsSource refactoring and serialization

--- modem/transmitter.py
+++ modem/transmitter.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
-from ruamel.yaml import RoundTripConstructor, Node
+from ruamel.yaml import SafeConstructor, Node
 from ruamel.yaml.comments import CommentedOrderedMap
 from typing import Type, List, TYPE_CHECKING
 
 from modem import Modem
+from source import BitsSource
 
 if TYPE_CHECKING:
     from scenario import Scenario
@@ -18,14 +19,19 @@ class Transmitter(Modem):
         Modem.__init__(self, scenario, **kwargs)
 
     @classmethod
-    def from_yaml(cls: Type[Transmitter], constructor: RoundTripConstructor, node: Node) -> Transmitter:
+    def from_yaml(cls: Type[Transmitter], constructor: SafeConstructor, node: Node) -> Transmitter:
 
         scenario = [scene for node, scene in constructor.constructed_objects.items() if node.tag == 'Scenario'][0]
 
-        mapping = constructor.construct_mapping(node, CommentedOrderedMap)
-        args = dict((k.lower(), v) for k, v in mapping.items())
+        state = constructor.construct_mapping(node, deep=True)
+        bits_source = state.pop(BitsSource.yaml_tag, None)
 
-        return Transmitter(scenario, **args)
+        args = dict((k.lower(), v) for k, v in state.items())
+        transmitter = Transmitter(scenario, **args)
+        yield transmitter
+
+        if bits_source is not None:
+            transmitter.bits_source = bits_source
 
     @property
     def index(self) -> int:
