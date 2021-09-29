654aa9c6029c53709840bfe0faeddef65ad55a41
jan.adler <jan.adler@barkhauseninstitut.org>
2021-09-29 20:10:41 +0200

    Interleaver testing refactored

--- tests/unit_tests/modem/test_interleaver.py
+++ tests/unit_tests/modem/test_interleaver.py
@@ -1,42 +1,107 @@
 import unittest
-
 import numpy as np
 
-from modem.coding.interleaver import BlockInterleaver
-from parameters_parser.parameters_block_interleaver import ParametersBlockInterleaver
-from .utils import assert_frame_equality
+from modem.coding.interleaver import Interleaver
 
 
 class TestBlockInterleaver(unittest.TestCase):
+
     def setUp(self) -> None:
-        self.M = 4
-        self.N = 3
-        self.bits_in_frame = 30
-        self.params = ParametersBlockInterleaver(self.M, self.N)
-        self.interleaver = BlockInterleaver(self.params, self.bits_in_frame)
 
-    def test_no_code_blocks(self) -> None:
-        self.assertTrue(self.interleaver.code_blocks, 2)
+        self.block_size = 12
+        self.interleave_blocks = 2
+        self.interleaver = Interleaver(self.block_size, self.interleave_blocks)
 
-    def test_source_bits(self) -> None:
-        self.assertTrue(self.interleaver.source_bits, 24)
+    def test_init(self) -> None:
+        """Test that the init properly stores all parameters."""
+
+        self.assertEquals(self.block_size, self.interleaver.block_size,
+                          "Block size init failed")
+        self.assertEquals(self.interleave_blocks, self.interleaver.interleave_blocks,
+                          "Number of interleaved blocks init failed")
+
+    def test_serialization(self) -> None:
+        """Serialization should recall the full object state.
+        TODO
+        """
+
+#        yaml = YAML(typ='safe')
+#        representer = SafeRepresenter()
+#        constructor = SafeConstructor()
+#
+#        yaml = Interleaver.to_yaml(representer, self.interleaver)
+#        recall = Interleaver.from_yaml(constructor, yaml)
+#
+#        self.assertEquals(self.interleaver.block_size, recall.block_size,
+#                          "Block size recall failed")
+#        self.assertEquals(self.interleaver.interleave_blocks, recall.interleave_blocks,
+#                          "Interleave blocks recall failed")
+
+    def test_init_validation(self) -> None:
+        """The interleaver init must raise a `ValueError` if blocks can't be sectioned properly"""
+
+        with self.assertRaises(ValueError):
+            self.interleaver.__init__(1, 2)
+
+    def test_bit_block_size(self) -> None:
+        """Bit block size must be equal to the configured block size."""
+
+        self.assertEquals(self.block_size, self.interleaver.bit_block_size)
+
+    def test_code_block_size(self) -> None:
+        """Code block size must be equal to the configured block size."""
+
+        self.assertEquals(self.block_size, self.interleaver.code_block_size)
+
+    def test_block_size(self) -> None:
+        """Block size getter must return setter value."""
+
+        self.interleaver.block_size = 1
+        self.assertEquals(1, self.interleaver.block_size)
+
+    def test_block_size_validation(self) -> None:
+        """Block size setter must throw ValueException on values smaller than one."""
+
+        with self.assertRaises(ValueError):
+            self.interleaver.block_size = 0
+
+        with self.assertRaises(ValueError):
+            self.interleaver.block_size = -1
+
+    def test_interleave_blocks(self) -> None:
+        """Interleave blocks getter must return setter value."""
+
+        self.interleaver.interleave_blocks = 1
+        self.assertEquals(1, self.interleaver.interleave_blocks)
+
+    def test_interleave_blocks_validation(self) -> None:
+        """Interleave blocks setter must throw ValueException on values smaller than one."""
+
+        with self.assertRaises(ValueError):
+            self.interleaver.interleave_blocks = 0
+
+        with self.assertRaises(ValueError):
+            self.interleaver.interleave_blocks = -1
+
+    def test_rate(self) -> None:
+        """Rate must always return 1.0"""
+
+        self.assertEquals(1.0, self.interleaver.rate, "Reported code rate is not 1.0")
 
     def test_interleaving(self) -> None:
-        data_bits = [np.arange(24)]
-        expected_encoded_bits = [
-            np.array([0, 3, 6, 9, 1, 4, 7, 10, 2, 5, 8, 11]),
-            np.array([12, 15, 18, 21, 13, 16, 19, 22, 14, 17, 20, 23]),
-            np.zeros(6)
-        ]
-        assert_frame_equality(expected_encoded_bits, self.interleaver.encode(data_bits))
+        """Interleaving must produce the expected results."""
+
+        bits = np.arange(self.block_size)
+        expected_code = np.array([0, 6, 1, 7, 2, 8, 3, 9, 4, 10, 5, 11])
+
+        np.testing.assert_array_equal(expected_code, self.interleaver.encode(bits),
+                                      "Interleaving produced unexpected code output")
 
     def test_deinterleaving(self) -> None:
-        interleaved_bits = [
-            np.array([0, 3, 6, 9, 1, 4, 7, 10, 2, 5, 8, 11]),
-            np.array([12, 15, 18, 21, 13, 16, 19, 22, 14, 17, 20, 23]),
-            np.zeros(6)
-        ]
-        expected_data_bits = [np.arange(12), np.arange(12, 24)]
-        assert_frame_equality(expected_data_bits, self.interleaver.decode(interleaved_bits))
+        """De-Interleaving must produce the expected results."""
 
+        code = np.array([0, 6, 1, 7, 2, 8, 3, 9, 4, 10, 5, 11])
+        expected_bits = np.arange(self.block_size)
 
+        np.testing.assert_array_equal(expected_bits, self.interleaver.decode(code),
+                                      "De-interleaving produced unexpected data bit sequence")
