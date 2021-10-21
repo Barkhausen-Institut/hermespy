7305322d08b78174cd52868734e5a71843827bb5
anollba <andre.nollbarreto@barkhauseninstitut.org>
2021-10-22 00:04:38 +0200

    Add RadarChannel to channel module

--- channel/__init__.py
+++ channel/__init__.py
@@ -1,6 +1,7 @@
 from .channel import Channel
 from .multipath_fading_channel import MultipathFadingChannel
 from .multipath_fading_templates import MultipathFadingCost256, MultipathFading5GTDL
+from .radar_channel import RadarChannel
 
 try:
     from .quadriga_interface_matlab import QuadrigaMatlabInterface as QuadrigaInterface
@@ -16,4 +17,4 @@ except ImportError:
 from .quadriga_channel import QuadrigaChannel
 
 __all__ = ['Channel', 'MultipathFadingChannel', 'MultipathFading5GTDL', 'MultipathFadingCost256',
-           'QuadrigaChannel', 'QuadrigaInterface']
+           'QuadrigaChannel', 'QuadrigaInterface', 'RadarChannel']
