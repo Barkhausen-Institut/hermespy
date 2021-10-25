62d35adf8a6007522ea88372751545d02df47346
Jan Adler <jan.adler@iwu.fraunhofer.de>
2021-10-25 11:40:23 +0200

    Moved send and receive routines to transmitter and receiver, respectively

--- modem/transmitter.py
+++ modem/transmitter.py
@@ -3,7 +3,7 @@
 
 from __future__ import annotations
 from ruamel.yaml import SafeConstructor, Node, MappingNode, ScalarNode
-from typing import Type, List, TYPE_CHECKING, Any
+from typing import Type, List, Any, Optional
 import numpy as np
 import numpy.random as rnd
 
@@ -11,8 +11,6 @@ from modem import Modem
 from source import BitsSource
 from modem.waveform_generator import WaveformGenerator
 
-if TYPE_CHECKING:
-    from scenario import Scenario
 
 __author__ = "Jan Adler"
 __copyright__ = "Copyright 2021, Barkhausen Institut gGmbH"
@@ -36,6 +34,64 @@ class Transmitter(Modem):
         """
 
         Modem.__init__(self, **kwargs)
+
+    def send(self,
+             drop_duration: Optional[float] = None,
+             data_bits: Optional[np.array] = None) -> np.ndarray:
+        """Returns an array with the complex baseband samples of a waveform generator.
+
+        The signal may be distorted by RF impairments.
+
+        Args:
+            drop_duration (float, optional): Length of signal in seconds.
+            data_bits (np.array, optional): Data bits to be sent via this transmitter.
+
+        Returns:
+            np.ndarray:
+                Complex baseband samples, rows denoting transmitter antennas and
+                columns denoting samples.
+
+        Raises:
+            ValueError: If not enough data bits were provided to generate as single frame.
+        """
+        # coded_bits = self.encoder.encoder(data_bits)
+        number_of_samples = int(np.ceil(drop_duration * self.sampling_rate))
+        timestamp = 0
+        frame_index = 1
+
+        num_code_bits = self.waveform_generator.bits_per_frame
+        num_data_bits = self.encoder_manager.required_num_data_bits(num_code_bits)
+
+        # Generate source data bits if none are provided
+        if data_bits is None:
+            data_bits = self.__bits_source.get_bits(num_data_bits)[0]
+
+        # Make sure enough data bits were provided
+        elif len(data_bits) < num_data_bits:
+            raise ValueError("Number of provided data bits is insufficient to generate a single frame")
+
+        # Apply channel coding to the source bits
+        code_bits = self.encoder_manager.encode(data_bits, num_code_bits)
+
+        while timestamp < number_of_samples:
+
+            # Generate base-band waveforms
+            frame, timestamp, initial_sample_num = self.waveform_generator.create_frame(
+                timestamp, code_bits)
+
+            if frame_index == 1:
+                tx_signal, samples_delay = self._allocate_drop_size(
+                    initial_sample_num, number_of_samples)
+
+            tx_signal, samples_delay = self._add_frame_to_drop(
+                initial_sample_num, samples_delay, tx_signal, frame)
+            frame_index += 1
+
+        tx_signal = self.rf_chain.send(tx_signal)
+        tx_signal = self._adjust_tx_power(tx_signal)
+        return tx_signal
+
+
 
     @classmethod
     def from_yaml(cls: Type[Transmitter], constructor: SafeConstructor, node: Node) -> Transmitter:
