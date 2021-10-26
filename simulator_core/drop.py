dd6354cc8b7b6a0aae03f9db1bce6d212d686565
Jan Adler <jan.adler@barkhauseninstitut.org>
2021-10-26 09:19:35 +0200

    Moved bit error rate computation to drop class

--- simulator_core/drop.py
+++ simulator_core/drop.py
@@ -2,7 +2,7 @@
 """HermesPy data drop."""
 
 from __future__ import annotations
-from typing import List
+from typing import List, Optional
 import numpy as np
 import matplotlib.pyplot as plt
 from enum import IntEnum
@@ -59,18 +59,49 @@ class ComplexVisualization(IntEnum):
 
 
 class Drop:
-    """Data generated within a single execution drop."""
+    """Data generated within a single execution drop.
+
+    Attributes:
+
+        __transmitted_bits (List[np.ndarray]):
+            Bits fed into the transmitting modems.
+
+        __transmitted_signals (List[np.ndarray]):
+            Modulated signals emitted by transmitting modems.
+
+        __received_signals (List[np.ndarray]):
+            Modulated signals impinging onto receiving modems.
+
+        __received_bits (List[np.ndarray]):
+            Bits output by receiving modems.
+
+        __bit_errors (Optional[List[List[Optional[np.ndarray]]]]):
+            Bit errors which occurred during transmission.
+            The attribute is None if the bit errors have not been calculated yet.
+            Contains a matrix of dimensions `num_transmitters`x`num_receivers`x`bit_deltas`.
+            The length `bit_deltas` is the maximum of all transmitted and received bits.
+            `bit_deltas` are optional, if a computation is not feasible the matrix field is set to None.
+
+        __pad_bit_errors (bool):
+            Controls behaviour during bit error computation.
+            If enabled, bit streams of different length between transmitter and receiver will be extended by zeros,
+            in order to match the maximum dimension and subsequently compared.
+            If disabled, bit streams of different length will be ignored during bit error computation.
+    """
 
     __transmitted_bits: List[np.ndarray]
     __transmitted_signals: List[np.ndarray]
     __received_signals: List[np.ndarray]
     __received_bits: List[np.ndarray]
+    __bit_errors: Optional[List[List[Optional[np.ndarray]]]]
+    __pad_bit_errors: bool
 
     def __init__(self,
                  transmitted_bits: List[np.ndarray],
                  transmitted_signals: List[np.ndarray],
                  received_signals: List[np.ndarray],
-                 received_bits: List[np.ndarray]) -> None:
+                 received_bits: List[np.ndarray],
+                 pad_bit_errors: bool = True) -> None:
         """Object initialization.
 
         Args:
@@ -78,12 +109,15 @@ class Drop:
             transmitted_signals (List[np.ndarray]): Modulated signals emitted by transmitting modems.
             received_signals (List[np.ndarray]): Modulated signals impinging onto receiving modems.
             received_bits (List[np.ndarray]): Bits output by receiving modems.
+            pad_bit_errors (bool, optional): Pad bit streams during error computation.
         """
 
         self.__transmitted_bits = transmitted_bits
         self.__transmitted_signals = transmitted_signals
         self.__received_signals = received_signals
         self.__received_bits = received_bits
+        self.__bit_errors = None
+        self.__pad_bit_errors = pad_bit_errors
 
     @property
     def transmitted_bits(self) -> List[np.ndarray]:
@@ -108,6 +142,48 @@ class Drop:
         """Access received bits."""
 
         return self.__received_bits
+
+    @property
+    def bit_errors(self) -> List[List[Optional[np.ndarray]]]:
+        """Detect the bit transmission deltas between all transmitting and receiving modems.
+
+        The computation result gets so buffered, meaning only the first call is computationally expensive.
+
+        Returns:
+            List[List[np.ndarray]]:
+                A numpy of dimensions `num_transmitters`x`num_receivers`x`bit_deltas`.
+                The length `bit_deltas` is the maximum of all transmitted and received bits.
+        """
+
+        # Return the cached result if already computed
+        if self.__bit_errors is not None:
+            return self.__bit_errors
+
+        self.__bit_errors: List[List[np.ndarray]] = []
+        for transmitter_bits in self.transmitted_bits:
+
+            transmit_errors: List[Optional[np.ndarray]] = []
+            for receiver_bits in self.received_bits:
+
+                # Skip error computation if padding is disabled and the stream lengths don't match
+                if len(receiver_bits) != len(transmitter_bits) and not self.__pad_bit_errors:
+                    transmit_errors.append(None)
+
+                else:
+
+                    # Pad bit sequences (if required)
+                    num_bits = max(len(receiver_bits), len(transmitter_bits))
+                    padded_transmission = np.append(transmitter_bits, np.zeros(num_bits - len(transmitter_bits)))
+                    padded_reception = np.append(receiver_bits, np.zeros(num_bits - len(receiver_bits)))
+
+                    # Compute bit errors as the positions where both sequences differ.
+                    # Note that this requires the sequences to be in 0/1 format!
+                    bit_errors = np.abs(padded_transmission - padded_reception)
+                    transmit_errors.append(bit_errors)
+
+            self.__bit_errors.append(transmit_errors)
+
+        return self.__bit_errors
 
     def plot_transmitted_signals(self,
                                  visualization: ComplexVisualization = ComplexVisualization.REAL) -> None:
@@ -227,26 +303,25 @@ class Drop:
         if grid_height < 1:
             return
 
+        # Fetch bit errors
+        bit_errors = self.bit_errors
+
+        # Plot bit errors
         figure, axes = plt.subplots(grid_height, grid_width, squeeze=False)
         figure.suptitle("Bit Errors")
 
-        for transimission_index, transmitted_bits in enumerate(self.__transmitted_bits):
-            for reception_index, received_bits in enumerate(self.__received_bits):
+        for transmission_index in range(len(self.__transmitted_bits)):
+            for reception_index in range(len(self.__received_bits)):
 
-                if len(received_bits) < 1 or len(received_bits) < 1:
-                    axes[transimission_index, reception_index].text(0, 0, "No bits exchanged")
+                link_errors: Optional[np.ndarray] = bit_errors[transmission_index][reception_index]
+
+                if link_errors is None:
+                    axes[transmission_index, reception_index].text(0, 0, "No bits exchanged")
 
                 else:
+                    axes[transmission_index, reception_index].stem(link_errors)
 
-                    num_bits = max(len(received_bits), len(transmitted_bits))
-                    padded_transmission = np.append(transmitted_bits, np.zeros(num_bits - len(transmitted_bits)))
-                    padded_reception = np.append(received_bits, np.zeros(num_bits - len(received_bits)))
-
-                    delta = np.abs(padded_transmission - padded_reception)
-
-                    axes[transimission_index, reception_index].stem(delta)
-
-            axes[transimission_index, 0].set(ylabel="Tx {}".format(transimission_index))
+            axes[transmission_index, 0].set(ylabel="Tx {}".format(transmission_index))
 
         for reception_index in range(len(self.received_bits)):
             axes[-1, reception_index].set(xlabel="Rx {}".format(reception_index))
