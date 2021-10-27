cb5a0071297321fda04e8475cd12168ef0d0ddaa
Jan Adler <jan.adler@barkhauseninstitut.org>
2021-10-27 13:56:43 +0200

    Re-introduced noise modeling

--- modem/__init__.py
+++ modem/__init__.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 from .modem import TransmissionMode, Modem
 from .transmitter import Transmitter
 from .receiver import Receiver
@@ -6,4 +5,4 @@ from .rf_chain import RfChain
 from .waveform_generator import WaveformGenerator
 from .waveform_generator_chirp_fsk import WaveformGeneratorChirpFsk
 
-__all__ = [Modem, Transmitter, Receiver, RfChain, WaveformGenerator, WaveformGeneratorChirpFsk]
+__all__ = ['Modem', 'Transmitter', 'Receiver', 'RfChain', 'WaveformGenerator', 'WaveformGeneratorChirpFsk']
