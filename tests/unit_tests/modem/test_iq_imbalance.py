4eed709fd9db2fc4f215c3f18de6aec27fb5ab3d
Tobias Kronauer <tobias.kronauer@barkhauseninstitut.org>
2021-11-03 10:50:08 +0100

    move yaml tests to tet_iq_imbalance module

--- tests/unit_tests/modem/test_iq_imbalance.py
+++ tests/unit_tests/modem/test_iq_imbalance.py
@@ -1,8 +1,10 @@
 import unittest
+import re
 
 import numpy as np
 
 from modem.rf_chain import RfChain
+from simulator_core.factory import Factory
 
 
 class TestIqImbalance(unittest.TestCase):
@@ -41,3 +43,46 @@ class TestIqImbalance(unittest.TestCase):
     def test_exception_raised_if_amplitude_imbalance_not_within_interval(self) -> None:
         with self.assertRaises(ValueError):
             rf_chain = RfChain(None, None, -3)
+
+
+class TestIqImbalanceCreationAndSerialization(unittest.TestCase):
+    def setUp(self) -> None:
+        self.factory = Factory()
+
+    def test_creation_proper_values(self) -> None:
+        AMPLITUDE_IMBALANCE = 0.5
+        PHASE_OFFSET = 3
+        yaml_str = f"""
+!<Scenario>
+
+Modems:
+  - Transmitter
+    RfChain:
+       amplitude_imbalance: {AMPLITUDE_IMBALANCE}
+       phase_offset: {PHASE_OFFSET}
+"""
+        scenarios = self.factory.from_str(yaml_str)
+        self.assertAlmostEqual(
+            scenarios[0].transmitters[0].rf_chain.amplitude_imbalance,
+            AMPLITUDE_IMBALANCE
+        )
+        self.assertEqual(
+            scenarios[0].transmitters[0].rf_chain.phase_offset,
+            PHASE_OFFSET
+        )
+
+    def test_iq_imbalance_serialisation(self) -> None:
+        PHASE_OFFSET = 10
+        AMPLITUDE_IMBALANCE = 0.5
+        rf_chain = RfChain(phase_offset=PHASE_OFFSET,
+                           amplitude_imbalance=AMPLITUDE_IMBALANCE)
+
+        serialized_rf_chain = self.factory.to_str(rf_chain)
+        phase_offset_regex = re.compile(
+            f'^phase_offset: {PHASE_OFFSET}$', re.MULTILINE)
+        amplitude_imbalance_regex = re.compile(
+            f'^amplitude_imbalance: {AMPLITUDE_IMBALANCE}$',
+            re.MULTILINE)
+
+        self.assertTrue(re.search(phase_offset_regex, serialized_rf_chain) is not None)
+        self.assertTrue(re.search(amplitude_imbalance_regex, serialized_rf_chain) is not None)
