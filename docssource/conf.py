b4b9e5106d5f3bb13fa63a75506e0438d31c75f2
Jan Adler <jan.adler@barkhauseninstitut.org>
2021-11-29 14:10:19 +0100

    RTD documentation theme

--- docssource/conf.py
+++ docssource/conf.py
@@ -76,7 +76,7 @@ autodoc_default_options = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
