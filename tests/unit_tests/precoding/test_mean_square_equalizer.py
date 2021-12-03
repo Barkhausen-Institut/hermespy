a37705b4a07185c929826d40837ab0fe6cbe12d3
Jan Adler <jan.adler@barkhauseninstitut.org>
2021-12-03 13:56:52 +0100

    Channel equalization unit testing

--- tests/unit_tests/precoding/test_mean_square_equalizer.py
+++ tests/unit_tests/precoding/test_mean_square_equalizer.py
@@ -8,6 +8,7 @@ import numpy as np
 from numpy.testing import assert_array_almost_equal
 from scipy.constants import pi
 
+from hermespy.channel import ChannelStateFormat, ChannelStateInformation
 from hermespy.precoding import MMSESpaceEqualizer
 
 __author__ = "Jan Adler"
@@ -49,23 +50,25 @@ class TestMMSESpaceEqualizer(unittest.TestCase):
         num_streams = 4
 
         expected_symbols = np.exp(1j * self.generator.uniform(0, 2*pi, (num_streams, num_samples)))
-        stream_responses = np.exp(1j * self.generator.uniform(0, 2*pi, (num_streams, num_samples, num_streams)))
+        stream_responses = np.exp(1j * self.generator.uniform(0, 2*pi, (num_streams, num_streams, num_samples, 1)))
+        channel_state = ChannelStateInformation(ChannelStateFormat.IMPULSE_RESPONSE, stream_responses)
 
         propagated_symbol_stream = np.empty((num_streams, num_samples), dtype=complex)
-        expected_equalized_responses = np.zeros((num_streams, num_samples, num_streams), dtype=complex)
-        for symbol_idx, (response, symbol) in enumerate(zip(np.rollaxis(stream_responses, 1), expected_symbols.T)):
+        expected_equalized_responses = np.zeros((num_streams, num_streams, num_samples, 1), dtype=complex)
+        for symbol_idx, (response, symbol) in enumerate(zip(stream_responses.transpose((2, 0, 1, 3)),
+                                                            expected_symbols.T)):
 
-            propagated_symbol_stream[:, symbol_idx] = response @ symbol
-            expected_equalized_responses[:, symbol_idx, :] = np.identity(num_streams, dtype=complex)
+            propagated_symbol_stream[:, symbol_idx] = response[:, :, 0] @ symbol
+            expected_equalized_responses[:, :, symbol_idx, 0] = np.identity(num_streams, dtype=complex)
 
         stream_noises = np.zeros((num_streams, num_samples), dtype=float)
 
-        equalized_symbols, equalized_responses, equalized_noises = self.precoder.decode(propagated_symbol_stream,
-                                                                                        stream_responses,
-                                                                                        stream_noises)
+        equalized_symbols, equalized_csi, equalized_noises = self.precoder.decode(propagated_symbol_stream,
+                                                                                  channel_state,
+                                                                                  stream_noises)
 
         assert_array_almost_equal(expected_symbols, equalized_symbols)
-        assert_array_almost_equal(expected_equalized_responses, equalized_responses)
+        assert_array_almost_equal(expected_equalized_responses, equalized_csi.state)
         assert_array_almost_equal(equalized_noises, np.zeros((num_streams, num_samples), dtype=float))
 
     def test_num_input_streams(self) -> None:
