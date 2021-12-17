2c2fbe0dd10accc0e22b1a117b425919e0b4dab0
Jan Adler <jan.adler@barkhauseninstitut.org>
2021-12-17 12:36:24 +0100

    Moved scenario to core

--- tests/unit_tests/scenario/test_scenario.py
+++ tests/unit_tests/scenario/test_scenario.py
@@ -9,7 +9,7 @@ from unittest.mock import Mock
 from itertools import product
 from numpy.testing import assert_array_equal
 
-from hermespy.scenario.scenario import Scenario
+from hermespy.core.scenario import Scenario
 
 __author__ = "Tobias Kronauer"
 __copyright__ = "Copyright 2021, Barkhausen Institut gGmbH"
