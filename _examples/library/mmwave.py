6e2b6d6d4ab55aff0442bbf19d217f6cc5aa72bb
Jan Adler <jan.adler@barkhauseninstitut.org>
2022-01-07 15:20:12 +0100

    Signal plotting improvements

--- _examples/library/mmwave.py
+++ _examples/library/mmwave.py
@@ -1,40 +1,39 @@
 import matplotlib.pyplot as plt
 
-from hermespy.hardware_loop.hardware_loop import HardwareLoop
 from hermespy.hardware_loop.ni_mmwave import NiMmWaveDevice
 from hermespy.modem.modem import Modem
 from hermespy.modem.waveform_generator_psk_qam import WaveformGeneratorPskQam, PskQamCorrelationSynchronization
 from hermespy.modem.evaluators import BitErrorEvaluator
 
-hardware_loop = HardwareLoop()
-
+# Initialize device binding
 device = NiMmWaveDevice('192.168.189.120')
 device.tx_gain_db = 0
 device.rx_gain_db = 50
 device.carrier_frequency = 75e9
 device.time_buffer = 1e-7
 
-hardware_loop.add_device(device)
-
+# Configure communication operator and waveform
 modem = Modem()
 waveform_generator = WaveformGeneratorPskQam(oversampling_factor=4)
 modem.waveform_generator = waveform_generator
 waveform_generator.num_preamble_symbols = 10
 waveform_generator.num_data_symbols = 50
 waveform_generator.modulation_order = 64
-waveform_generator.synchronization = PskQamCorrelationSynchronization()
+# waveform_generator.synchronization = PskQamCorrelationSynchronization()
 device.sampling_rate = waveform_generator.sampling_rate
 
 modem.device = device
 
-bit_error = BitErrorEvaluator(modem, modem)
-
+# Transmit and receive waveforms over device
 tx_signal, tx_symbols, tx_bits = modem.transmit()
 device.trigger()
 rx_signal, rx_symbols, rx_bits = modem.receive()
 
-bit_error.evaluate().plot()
+# Compute bit errors
+bit_error = BitErrorEvaluator(modem, modem)
 
-tx_signal.plot()
-rx_signal.plot()
+# Visualize results
+bit_error.evaluate().plot()
+tx_signal.plot(title="Tx Signal")
+rx_signal.plot(title="Rx Signal")
 plt.show()
