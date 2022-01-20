10080088eb5ab5283637736de843a5d5a55907d7
anollba <andre.nollbarreto@barkhauseninstitut.org>
2022-01-20 18:37:15 +0100

    Correct authors in tests

--- tests/unit_tests/modem/test_waveform_generator_chirp_fsk.py
+++ tests/unit_tests/modem/test_waveform_generator_chirp_fsk.py
@@ -13,9 +13,9 @@ from hermespy.channel import ChannelStateInformation
 from hermespy.modem.waveform_generator_chirp_fsk import WaveformGeneratorChirpFsk, ChirpFskSynchronization,\
     ChirpFskCorrelationSynchronization
 
-__author__ = "Tobias Kronauer"
+__author__ = "Andre Noll Barreto"
 __copyright__ = "Copyright 2021, Barkhausen Institut gGmbH"
-__credits__ = ["Tobias Kronauer", "Jan Adler"]
+__credits__ = ["Andre Noll Barreto", "Tobias Kronauer", "Jan Adler"]
 __license__ = "AGPLv3"
 __version__ = "0.1.0"
 __maintainer__ = "Jan Adler"
--- tests/unit_tests/noise/test_noise.py
+++ tests/unit_tests/noise/test_noise.py
@@ -9,9 +9,9 @@ import numpy.random as rnd
 
 from hermespy.noise import Noise
 
-__author__ = "Tobias Kronauer"
+__author__ = "Andre Noll Barreto"
 __copyright__ = "Copyright 2021, Barkhausen Institut gGmbH"
-__credits__ = ["Tobias Kronauer", "Jan Adler"]
+__credits__ = ["Andre Noll Barreto", "Tobias Kronauer", "Jan Adler"]
 __license__ = "AGPLv3"
 __version__ = "0.2.3"
 __maintainer__ = "Jan Adler"
