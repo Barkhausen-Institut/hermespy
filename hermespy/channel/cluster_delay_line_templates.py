ca430ef9ec8eaaa3840efa3a386c43edd4afb98d
Jan Adler <jan.adler@barkhauseninstitut.org>
2022-02-03 21:42:07 +0100

    Urban Macrocells CDL Templates

--- hermespy/channel/cluster_delay_line_templates.py
+++ hermespy/channel/cluster_delay_line_templates.py
@@ -106,8 +106,7 @@ class StreetCanyonLineOfSight(ClusterDelayLineBase, Serializable):
 
     @property
     def _center_frequency(self) -> float:
-
-        return 2e9
+        return max(2e9, ClusterDelayLineBase._center_frequency.fget(self))
 
 
 class StreetCanyonNonLineOfSight(ClusterDelayLineBase, Serializable):
@@ -202,5 +201,384 @@ class StreetCanyonNonLineOfSight(ClusterDelayLineBase, Serializable):
 
     @property
     def _center_frequency(self) -> float:
+        return max(2e9, ClusterDelayLineBase._center_frequency.fget(self))
 
-        return 2e9
+
+class StreetCanyonOutsideToInside(ClusterDelayLineBase, Serializable):
+    """Parameter Preset for the 3GPP Cluster Delay Line Urban Microcells Street Canyon Model."""
+
+    yaml_tag = u'StreetCanyonO2I'
+    """YAML serialization tag."""
+
+    @property
+    def line_of_sight(self) -> bool:
+        return False
+
+    @property
+    def delay_spread_mean(self) -> float:
+        return -6.62
+
+    @property
+    def delay_spread_std(self) -> float:
+        return .32
+
+    @property
+    def aod_spread_mean(self) -> float:
+        return 1.25
+
+    @property
+    def aod_spread_std(self) -> float:
+        return .42
+
+    @property
+    def aoa_spread_mean(self) -> float:
+        return 1.76
+
+    @property
+    def aoa_spread_std(self) -> float:
+        return .16
+
+    @property
+    def zoa_spread_mean(self) -> float:
+        return 1.01
+
+    @property
+    def zoa_spread_std(self) -> float:
+        return .43
+
+    @property
+    def rice_factor_mean(self) -> float:
+        return 0.
+
+    @property
+    def rice_factor_std(self) -> float:
+        return 0.
+
+    @property
+    def delay_scaling(self) -> float:
+        return 2.2
+
+    @property
+    def cross_polarization_power_mean(self) -> float:
+        return 9.
+
+    @property
+    def cross_polarization_power_std(self) -> float:
+        return 5.
+
+    @property
+    def num_clusters(self) -> int:
+        return 12
+
+    @property
+    def num_rays(self) -> int:
+        return 20
+
+    @property
+    def cluster_delay_spread(self) -> float:
+        return 11.
+
+    @property
+    def cluster_aod_spread(self) -> float:
+        return 5.
+
+    @property
+    def cluster_aoa_spread(self) -> float:
+        return 8.
+
+    @property
+    def cluster_zoa_spread(self) -> float:
+        return 3.
+
+    @property
+    def cluster_shadowing_std(self) -> float:
+        return 4.
+
+    @property
+    def _center_frequency(self) -> float:
+        return max(2e9, ClusterDelayLineBase._center_frequency.fget(self))
+
+
+class UrbanMacrocellsLineOfSight(ClusterDelayLineBase, Serializable):
+    """Parameter Preset for the 3GPP Cluster Delay Line Urban Macrocells Model."""
+
+    yaml_tag = u'UMaLOS'
+    """YAML serialization tag."""
+
+    @property
+    def line_of_sight(self) -> bool:
+        return True
+
+    @property
+    def delay_spread_mean(self) -> float:
+        return -6.995 - .0963 * log10(self._center_frequency * 1e-9)
+
+    @property
+    def delay_spread_std(self) -> float:
+        return .66
+
+    @property
+    def aod_spread_mean(self) -> float:
+        return 1.06 + .1114 * log10(self._center_frequency * 1e-9)
+
+    @property
+    def aod_spread_std(self) -> float:
+        return .28
+
+    @property
+    def aoa_spread_mean(self) -> float:
+        return 1.81
+
+    @property
+    def aoa_spread_std(self) -> float:
+        return .2
+
+    @property
+    def zoa_spread_mean(self) -> float:
+        return .95
+
+    @property
+    def zoa_spread_std(self) -> float:
+        return .16
+
+    @property
+    def rice_factor_mean(self) -> float:
+        return 9.
+
+    @property
+    def rice_factor_std(self) -> float:
+        return 3.5
+
+    @property
+    def delay_scaling(self) -> float:
+        return 2.5
+
+    @property
+    def cross_polarization_power_mean(self) -> float:
+        return 8.
+
+    @property
+    def cross_polarization_power_std(self) -> float:
+        return 4.
+
+    @property
+    def num_clusters(self) -> int:
+        return 12
+
+    @property
+    def num_rays(self) -> int:
+        return 20
+
+    @property
+    def cluster_delay_spread(self) -> float:
+        return max(.25, 6.5622 - 3.4084 * log10(self._center_frequency * 1e-9))
+
+    @property
+    def cluster_aod_spread(self) -> float:
+        return 5.
+
+    @property
+    def cluster_aoa_spread(self) -> float:
+        return 11.
+
+    @property
+    def cluster_zoa_spread(self) -> float:
+        return 7.
+
+    @property
+    def cluster_shadowing_std(self) -> float:
+        return 3.
+
+    @property
+    def _center_frequency(self) -> float:
+        return max(6e9, ClusterDelayLineBase._center_frequency.fget(self))
+
+
+class UrbanMacrocellsNoneLineOfSight(ClusterDelayLineBase, Serializable):
+    """Parameter Preset for the 3GPP Cluster Delay Line Urban Macrocells Model."""
+
+    yaml_tag = u'UMaNLOS'
+    """YAML serialization tag."""
+
+    @property
+    def line_of_sight(self) -> bool:
+        return False
+
+    @property
+    def delay_spread_mean(self) -> float:
+        return -6.28 - .204 * log10(self._center_frequency * 1e-9)
+
+    @property
+    def delay_spread_std(self) -> float:
+        return .39
+
+    @property
+    def aod_spread_mean(self) -> float:
+        return 1.5 + .1114 * log10(self._center_frequency * 1e-9)
+
+    @property
+    def aod_spread_std(self) -> float:
+        return .28
+
+    @property
+    def aoa_spread_mean(self) -> float:
+        return 2.08 - .27 * log10(self._center_frequency * 1e-9)
+
+    @property
+    def aoa_spread_std(self) -> float:
+        return .11
+
+    @property
+    def zoa_spread_mean(self) -> float:
+        return -.3236 * log10(self._center_frequency * 1e-9) + 1.512
+
+    @property
+    def zoa_spread_std(self) -> float:
+        return .16
+
+    @property
+    def rice_factor_mean(self) -> float:
+        return 0.
+
+    @property
+    def rice_factor_std(self) -> float:
+        return 0.
+
+    @property
+    def delay_scaling(self) -> float:
+        return 2.3
+
+    @property
+    def cross_polarization_power_mean(self) -> float:
+        return 7.
+
+    @property
+    def cross_polarization_power_std(self) -> float:
+        return 3.
+
+    @property
+    def num_clusters(self) -> int:
+        return 20
+
+    @property
+    def num_rays(self) -> int:
+        return 20
+
+    @property
+    def cluster_delay_spread(self) -> float:
+        return max(.25, 6.5622 - 3.4084 * log10(self._center_frequency * 1e-9))
+
+    @property
+    def cluster_aod_spread(self) -> float:
+        return 2.
+
+    @property
+    def cluster_aoa_spread(self) -> float:
+        return 15.
+
+    @property
+    def cluster_zoa_spread(self) -> float:
+        return 7.
+
+    @property
+    def cluster_shadowing_std(self) -> float:
+        return 3.
+
+    @property
+    def _center_frequency(self) -> float:
+        return max(6e9, ClusterDelayLineBase._center_frequency.fget(self))
+
+
+class UrbanMacrocellsOutsideToInside(ClusterDelayLineBase, Serializable):
+    """Parameter Preset for the 3GPP Cluster Delay Line Urban Macrocells Model."""
+
+    yaml_tag = u'UMa02I'
+    """YAML serialization tag."""
+
+    @property
+    def line_of_sight(self) -> bool:
+        return False
+
+    @property
+    def delay_spread_mean(self) -> float:
+        return -6.62
+
+    @property
+    def delay_spread_std(self) -> float:
+        return .32
+
+    @property
+    def aod_spread_mean(self) -> float:
+        return 1.25
+
+    @property
+    def aod_spread_std(self) -> float:
+        return .42
+
+    @property
+    def aoa_spread_mean(self) -> float:
+        return 1.76
+
+    @property
+    def aoa_spread_std(self) -> float:
+        return .16
+
+    @property
+    def zoa_spread_mean(self) -> float:
+        return 1.01
+
+    @property
+    def zoa_spread_std(self) -> float:
+        return .43
+
+    @property
+    def rice_factor_mean(self) -> float:
+        return 0.
+
+    @property
+    def rice_factor_std(self) -> float:
+        return 0.
+
+    @property
+    def delay_scaling(self) -> float:
+        return 2.2
+
+    @property
+    def cross_polarization_power_mean(self) -> float:
+        return 9.
+
+    @property
+    def cross_polarization_power_std(self) -> float:
+        return 5.
+
+    @property
+    def num_clusters(self) -> int:
+        return 12
+
+    @property
+    def num_rays(self) -> int:
+        return 20
+
+    @property
+    def cluster_delay_spread(self) -> float:
+        return 11.
+
+    @property
+    def cluster_aod_spread(self) -> float:
+        return 5.
+
+    @property
+    def cluster_aoa_spread(self) -> float:
+        return 8.
+
+    @property
+    def cluster_zoa_spread(self) -> float:
+        return 3.
+
+    @property
+    def cluster_shadowing_std(self) -> float:
+        return 4.
+
+    @property
+    def _center_frequency(self) -> float:
+        return max(6e9, ClusterDelayLineBase._center_frequency.fget(self))
--- tests/unit_tests/channel/test_cluster_delay_line_templates.py
+++ tests/unit_tests/channel/test_cluster_delay_line_templates.py
@@ -11,7 +11,9 @@ from unittest.mock import Mock
 import numpy as np
 from numpy.random import default_rng
 
-from hermespy.channel.cluster_delay_line_templates import StreetCanyonLineOfSight, StreetCanyonNonLineOfSight
+from hermespy.channel.cluster_delay_line_templates import StreetCanyonLineOfSight, StreetCanyonNonLineOfSight,\
+    StreetCanyonOutsideToInside, UrbanMacrocellsLineOfSight, UrbanMacrocellsNoneLineOfSight, \
+    UrbanMacrocellsOutsideToInside
 
 __author__ = "Jan Adler"
 __copyright__ = "Copyright 2022, Barkhausen Institut gGmbH"
@@ -88,6 +90,158 @@ class TestStreetCanyonNLOS(TestCase):
 
         self.channel = StreetCanyonNonLineOfSight(receiver=self.receiver,
                                                   transmitter=self.transmitter)
+        self.channel.random_mother = self.random_node
+
+    def test_impulse_response(self):
+
+        num_samples = 5000
+        sampling_rate = 1e8
+
+        impulse_response = self.channel.impulse_response(num_samples, sampling_rate)
+        return
+
+
+class TestStreetCanyonO2I(TestCase):
+    """Test the 3GPP Cluster Delay Line Model Implementation."""
+
+    def setUp(self) -> None:
+
+        self.rng = default_rng(42)
+        self.random_node = Mock()
+        self.random_node._rng = self.rng
+
+        self.carrier_frequency = 1e9
+
+        self.receiver = Mock()
+        self.receiver.num_antennas = 1
+        self.receiver.position = np.array([100., 0., 0.])
+        self.receiver.antenna_positions = np.array([[100., 0., 0.]], dtype=float)
+        self.receiver.velocity = np.array([0., 0., 0.], dtype=float)
+        self.receiver.carrier_frequency = self.carrier_frequency
+
+        self.transmitter = Mock()
+        self.transmitter.num_antennas = 1
+        self.transmitter.position = np.array([-100., 0., 0.])
+        self.transmitter.antenna_positions = np.array([[-100., 0., 0.]], dtype=float)
+        self.transmitter.velocity = np.array([0., 0., 0.], dtype=float)
+        self.transmitter.carrier_frequency = 1e9
+
+        self.channel = StreetCanyonOutsideToInside(receiver=self.receiver,
+                                                   transmitter=self.transmitter)
+        self.channel.random_mother = self.random_node
+
+    def test_impulse_response(self):
+
+        num_samples = 5000
+        sampling_rate = 1e8
+
+        impulse_response = self.channel.impulse_response(num_samples, sampling_rate)
+        return
+
+
+class TestUrbanMacrocellsLOS(TestCase):
+    """Test the 3GPP Cluster Delay Line Model Implementation."""
+
+    def setUp(self) -> None:
+
+        self.rng = default_rng(42)
+        self.random_node = Mock()
+        self.random_node._rng = self.rng
+
+        self.carrier_frequency = 1e9
+
+        self.receiver = Mock()
+        self.receiver.num_antennas = 1
+        self.receiver.position = np.array([100., 0., 0.])
+        self.receiver.antenna_positions = np.array([[100., 0., 0.]], dtype=float)
+        self.receiver.velocity = np.array([0., 0., 0.], dtype=float)
+        self.receiver.carrier_frequency = self.carrier_frequency
+
+        self.transmitter = Mock()
+        self.transmitter.num_antennas = 1
+        self.transmitter.position = np.array([-100., 0., 0.])
+        self.transmitter.antenna_positions = np.array([[-100., 0., 0.]], dtype=float)
+        self.transmitter.velocity = np.array([0., 0., 0.], dtype=float)
+        self.transmitter.carrier_frequency = 1e9
+
+        self.channel = UrbanMacrocellsLineOfSight(receiver=self.receiver,
+                                                  transmitter=self.transmitter)
+        self.channel.random_mother = self.random_node
+
+    def test_impulse_response(self):
+
+        num_samples = 5000
+        sampling_rate = 1e8
+
+        impulse_response = self.channel.impulse_response(num_samples, sampling_rate)
+        return
+
+
+class TestUrbanMacrocellsNLOS(TestCase):
+    """Test the 3GPP Cluster Delay Line Model Implementation."""
+
+    def setUp(self) -> None:
+
+        self.rng = default_rng(42)
+        self.random_node = Mock()
+        self.random_node._rng = self.rng
+
+        self.carrier_frequency = 1e9
+
+        self.receiver = Mock()
+        self.receiver.num_antennas = 1
+        self.receiver.position = np.array([100., 0., 0.])
+        self.receiver.antenna_positions = np.array([[100., 0., 0.]], dtype=float)
+        self.receiver.velocity = np.array([0., 0., 0.], dtype=float)
+        self.receiver.carrier_frequency = self.carrier_frequency
+
+        self.transmitter = Mock()
+        self.transmitter.num_antennas = 1
+        self.transmitter.position = np.array([-100., 0., 0.])
+        self.transmitter.antenna_positions = np.array([[-100., 0., 0.]], dtype=float)
+        self.transmitter.velocity = np.array([0., 0., 0.], dtype=float)
+        self.transmitter.carrier_frequency = 1e9
+
+        self.channel = UrbanMacrocellsNoneLineOfSight(receiver=self.receiver,
+                                                      transmitter=self.transmitter)
+        self.channel.random_mother = self.random_node
+
+    def test_impulse_response(self):
+
+        num_samples = 5000
+        sampling_rate = 1e8
+
+        impulse_response = self.channel.impulse_response(num_samples, sampling_rate)
+        return
+
+
+class TestUrbanMacrocellsO2I(TestCase):
+    """Test the 3GPP Cluster Delay Line Model Implementation."""
+
+    def setUp(self) -> None:
+
+        self.rng = default_rng(42)
+        self.random_node = Mock()
+        self.random_node._rng = self.rng
+
+        self.carrier_frequency = 1e9
+
+        self.receiver = Mock()
+        self.receiver.num_antennas = 1
+        self.receiver.position = np.array([100., 0., 0.])
+        self.receiver.antenna_positions = np.array([[100., 0., 0.]], dtype=float)
+        self.receiver.velocity = np.array([0., 0., 0.], dtype=float)
+        self.receiver.carrier_frequency = self.carrier_frequency
+
+        self.transmitter = Mock()
+        self.transmitter.num_antennas = 1
+        self.transmitter.position = np.array([-100., 0., 0.])
+        self.transmitter.antenna_positions = np.array([[-100., 0., 0.]], dtype=float)
+        self.transmitter.velocity = np.array([0., 0., 0.], dtype=float)
+        self.transmitter.carrier_frequency = 1e9
+
+        self.channel = UrbanMacrocellsOutsideToInside(receiver=self.receiver,
+                                                      transmitter=self.transmitter)
         self.channel.random_mother = self.random_node
 
     def test_impulse_response(self):
