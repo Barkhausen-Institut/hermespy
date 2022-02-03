f53a6963d807994479893e8b0d8cc4214a08db28
Jan Adler <jan.adler@barkhauseninstitut.org>
2022-02-03 22:06:40 +0100

    Indoor Office CDL Templates

--- hermespy/channel/cluster_delay_line_templates.py
+++ hermespy/channel/cluster_delay_line_templates.py
@@ -394,7 +394,7 @@ class UrbanMacrocellsLineOfSight(ClusterDelayLineBase, Serializable):
         return max(6e9, ClusterDelayLineBase._center_frequency.fget(self))
 
 
-class UrbanMacrocellsNoneLineOfSight(ClusterDelayLineBase, Serializable):
+class UrbanMacrocellsNoLineOfSight(ClusterDelayLineBase, Serializable):
     """Parameter Preset for the 3GPP Cluster Delay Line Urban Macrocells Model."""
 
     yaml_tag = u'UMaNLOS'
@@ -674,8 +674,12 @@ class RuralMacrocellsLineOfSight(ClusterDelayLineBase, Serializable):
     def cluster_shadowing_std(self) -> float:
         return 3.
 
+    @property
+    def _center_frequency(self) -> float:
+        return max(6e9, ClusterDelayLineBase._center_frequency.fget(self))
 
-class RuralMacrocellsNoneLineOfSight(ClusterDelayLineBase, Serializable):
+
+class RuralMacrocellsNoLineOfSight(ClusterDelayLineBase, Serializable):
     """Parameter Preset for the 3GPP Cluster Delay Line Urban Macrocells Model."""
 
     yaml_tag = u'RMaLOS'
@@ -764,6 +768,10 @@ class RuralMacrocellsNoneLineOfSight(ClusterDelayLineBase, Serializable):
     @property
     def cluster_shadowing_std(self) -> float:
         return 3.
+
+    @property
+    def _center_frequency(self) -> float:
+        return max(6e9, ClusterDelayLineBase._center_frequency.fget(self))
 
 
 class RuralMacrocellsOutsideToInside(ClusterDelayLineBase, Serializable):
@@ -855,3 +863,197 @@ class RuralMacrocellsOutsideToInside(ClusterDelayLineBase, Serializable):
     @property
     def cluster_shadowing_std(self) -> float:
         return 3.
+
+    @property
+    def _center_frequency(self) -> float:
+        return max(6e9, ClusterDelayLineBase._center_frequency.fget(self))
+
+
+class IndoorOfficeLineOfSight(ClusterDelayLineBase, Serializable):
+    """Parameter Preset for the 3GPP Cluster Indoor-Office Model."""
+
+    yaml_tag = u'IndoorOfficeLOS'
+    """YAML serialization tag."""
+
+    @property
+    def line_of_sight(self) -> bool:
+        return True
+
+    @property
+    def delay_spread_mean(self) -> float:
+        return -.01 * log10(1 + self._center_frequency * 1e-9) - 7.692
+
+    @property
+    def delay_spread_std(self) -> float:
+        return .18
+
+    @property
+    def aod_spread_mean(self) -> float:
+        return 1.6
+
+    @property
+    def aod_spread_std(self) -> float:
+        return .18
+
+    @property
+    def aoa_spread_mean(self) -> float:
+        return -.19 * log10(1 + self._center_frequency * 1e-9) + 1.781
+
+    @property
+    def aoa_spread_std(self) -> float:
+        return .12 * log10(1 + self._center_frequency * 1e-9) + .119
+
+    @property
+    def zoa_spread_mean(self) -> float:
+        return -.26 * log10(1 + self._center_frequency * 1e-9) + 1.44
+
+    @property
+    def zoa_spread_std(self) -> float:
+        return -.04 * log10(1 + self._center_frequency * 1e-9) + .264
+
+    @property
+    def rice_factor_mean(self) -> float:
+        return 7.
+
+    @property
+    def rice_factor_std(self) -> float:
+        return 4.
+
+    @property
+    def delay_scaling(self) -> float:
+        return 3.6
+
+    @property
+    def cross_polarization_power_mean(self) -> float:
+        return 11.
+
+    @property
+    def cross_polarization_power_std(self) -> float:
+        return 4.
+
+    @property
+    def num_clusters(self) -> int:
+        return 15
+
+    @property
+    def num_rays(self) -> int:
+        return 20
+
+    @property
+    def cluster_delay_spread(self) -> float:
+        return 0.
+
+    @property
+    def cluster_aod_spread(self) -> float:
+        return 5.
+
+    @property
+    def cluster_aoa_spread(self) -> float:
+        return 8.
+
+    @property
+    def cluster_zoa_spread(self) -> float:
+        return 9.
+
+    @property
+    def cluster_shadowing_std(self) -> float:
+        return 6.
+
+    @property
+    def _center_frequency(self) -> float:
+        return max(6e9, ClusterDelayLineBase._center_frequency.fget(self))
+
+
+class IndoorOfficeNoLineOfSight(ClusterDelayLineBase, Serializable):
+    """Parameter Preset for the 3GPP Cluster Indoor-Office Model."""
+
+    yaml_tag = u'IndoorOfficeNLOS'
+    """YAML serialization tag."""
+
+    @property
+    def line_of_sight(self) -> bool:
+        return False
+
+    @property
+    def delay_spread_mean(self) -> float:
+        return -.28 * log10(1 + self._center_frequency * 1e-9) - 7.173
+
+    @property
+    def delay_spread_std(self) -> float:
+        return .1 * log10(1 + self._center_frequency * 1e-9) - .055
+
+    @property
+    def aod_spread_mean(self) -> float:
+        return 1.62
+
+    @property
+    def aod_spread_std(self) -> float:
+        return .25
+
+    @property
+    def aoa_spread_mean(self) -> float:
+        return -.11 * log10(1 + self._center_frequency * 1e-9) + 1.863
+
+    @property
+    def aoa_spread_std(self) -> float:
+        return .12 * log10(1 + self._center_frequency * 1e-9) + .119
+
+    @property
+    def zoa_spread_mean(self) -> float:
+        return -.15 * log10(1 + self._center_frequency * 1e-9) + 1.387
+
+    @property
+    def zoa_spread_std(self) -> float:
+        return -.09 * log10(1 + self._center_frequency * 1e-9) + .746
+
+    @property
+    def rice_factor_mean(self) -> float:
+        return 0.
+
+    @property
+    def rice_factor_std(self) -> float:
+        return 0.
+
+    @property
+    def delay_scaling(self) -> float:
+        return 3
+
+    @property
+    def cross_polarization_power_mean(self) -> float:
+        return 10.
+
+    @property
+    def cross_polarization_power_std(self) -> float:
+        return 4.
+
+    @property
+    def num_clusters(self) -> int:
+        return 19
+
+    @property
+    def num_rays(self) -> int:
+        return 20
+
+    @property
+    def cluster_delay_spread(self) -> float:
+        return 0.
+
+    @property
+    def cluster_aod_spread(self) -> float:
+        return 5.
+
+    @property
+    def cluster_aoa_spread(self) -> float:
+        return 11.
+
+    @property
+    def cluster_zoa_spread(self) -> float:
+        return 9.
+
+    @property
+    def cluster_shadowing_std(self) -> float:
+        return 3.
+
+    @property
+    def _center_frequency(self) -> float:
+        return max(6e9, ClusterDelayLineBase._center_frequency.fget(self))
--- tests/unit_tests/channel/test_cluster_delay_line_templates.py
+++ tests/unit_tests/channel/test_cluster_delay_line_templates.py
@@ -12,9 +12,9 @@ import numpy as np
 from numpy.random import default_rng
 
 from hermespy.channel.cluster_delay_line_templates import StreetCanyonLineOfSight, StreetCanyonNonLineOfSight,\
-    StreetCanyonOutsideToInside, UrbanMacrocellsLineOfSight, UrbanMacrocellsNoneLineOfSight, \
-    UrbanMacrocellsOutsideToInside, RuralMacrocellsLineOfSight, RuralMacrocellsNoneLineOfSight, \
-    RuralMacrocellsOutsideToInside
+    StreetCanyonOutsideToInside, UrbanMacrocellsLineOfSight, UrbanMacrocellsNoLineOfSight, \
+    UrbanMacrocellsOutsideToInside, RuralMacrocellsLineOfSight, RuralMacrocellsNoLineOfSight, \
+    RuralMacrocellsOutsideToInside, IndoorOfficeLineOfSight, IndoorOfficeNoLineOfSight
 
 __author__ = "Jan Adler"
 __copyright__ = "Copyright 2022, Barkhausen Institut gGmbH"
@@ -203,7 +203,7 @@ class TestUrbanMacrocellsNLOS(TestCase):
         self.transmitter.velocity = np.array([0., 0., 0.], dtype=float)
         self.transmitter.carrier_frequency = 1e9
 
-        self.channel = UrbanMacrocellsNoneLineOfSight(receiver=self.receiver,
+        self.channel = UrbanMacrocellsNoLineOfSight(receiver=self.receiver,
                                                       transmitter=self.transmitter)
         self.channel.random_mother = self.random_node
 
@@ -317,7 +317,7 @@ class TestRuralMacrocellsNLOS(TestCase):
         self.transmitter.velocity = np.array([0., 0., 0.], dtype=float)
         self.transmitter.carrier_frequency = 1e9
 
-        self.channel = RuralMacrocellsNoneLineOfSight(receiver=self.receiver,
+        self.channel = RuralMacrocellsNoLineOfSight(receiver=self.receiver,
                                                       transmitter=self.transmitter)
         self.channel.random_mother = self.random_node
 
@@ -357,6 +357,82 @@ class TestRuralMacrocellsO2I(TestCase):
 
         self.channel = RuralMacrocellsOutsideToInside(receiver=self.receiver,
                                                       transmitter=self.transmitter)
+        self.channel.random_mother = self.random_node
+
+    def test_impulse_response(self):
+
+        num_samples = 5000
+        sampling_rate = 1e8
+
+        impulse_response = self.channel.impulse_response(num_samples, sampling_rate)
+        return
+
+
+class TestIndoorOfficeLOS(TestCase):
+    """Test the 3GPP Cluster Delay Line Model Implementation."""
+
+    def setUp(self) -> None:
+
+        self.rng = default_rng(42)
+        self.random_node = Mock()
+        self.random_node._rng = self.rng
+
+        self.carrier_frequency = 1e9
+
+        self.receiver = Mock()
+        self.receiver.num_antennas = 1
+        self.receiver.position = np.array([100., 0., 0.])
+        self.receiver.antenna_positions = np.array([[100., 0., 0.]], dtype=float)
+        self.receiver.velocity = np.array([0., 0., 0.], dtype=float)
+        self.receiver.carrier_frequency = self.carrier_frequency
+
+        self.transmitter = Mock()
+        self.transmitter.num_antennas = 1
+        self.transmitter.position = np.array([-100., 0., 0.])
+        self.transmitter.antenna_positions = np.array([[-100., 0., 0.]], dtype=float)
+        self.transmitter.velocity = np.array([0., 0., 0.], dtype=float)
+        self.transmitter.carrier_frequency = 1e9
+
+        self.channel = IndoorOfficeLineOfSight(receiver=self.receiver,
+                                               transmitter=self.transmitter)
+        self.channel.random_mother = self.random_node
+
+    def test_impulse_response(self):
+
+        num_samples = 5000
+        sampling_rate = 1e8
+
+        impulse_response = self.channel.impulse_response(num_samples, sampling_rate)
+        return
+
+
+class TestIndoorOfficeNLOS(TestCase):
+    """Test the 3GPP Cluster Delay Line Model Implementation."""
+
+    def setUp(self) -> None:
+
+        self.rng = default_rng(42)
+        self.random_node = Mock()
+        self.random_node._rng = self.rng
+
+        self.carrier_frequency = 1e9
+
+        self.receiver = Mock()
+        self.receiver.num_antennas = 1
+        self.receiver.position = np.array([100., 0., 0.])
+        self.receiver.antenna_positions = np.array([[100., 0., 0.]], dtype=float)
+        self.receiver.velocity = np.array([0., 0., 0.], dtype=float)
+        self.receiver.carrier_frequency = self.carrier_frequency
+
+        self.transmitter = Mock()
+        self.transmitter.num_antennas = 1
+        self.transmitter.position = np.array([-100., 0., 0.])
+        self.transmitter.antenna_positions = np.array([[-100., 0., 0.]], dtype=float)
+        self.transmitter.velocity = np.array([0., 0., 0.], dtype=float)
+        self.transmitter.carrier_frequency = 1e9
+
+        self.channel = IndoorOfficeNoLineOfSight(receiver=self.receiver,
+                                                   transmitter=self.transmitter)
         self.channel.random_mother = self.random_node
 
     def test_impulse_response(self):
