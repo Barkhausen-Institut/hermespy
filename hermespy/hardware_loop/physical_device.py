2208edce24e353bcc1d24bf10454b0c9074ca1cf
Jan Adler <jan.adler@barkhauseninstitut.org>
2022-02-10 10:44:34 +0100

    First iteration of a delay-compensating calibration routine

--- hermespy/hardware_loop/physical_device.py
+++ hermespy/hardware_loop/physical_device.py
@@ -6,6 +6,7 @@ Physical Devices
 """
 
 from __future__ import annotations
+from pickle import dump, load
 
 import numpy as np
 from abc import abstractmethod
@@ -79,8 +80,60 @@ class PowerReceiver(Receiver):
         return 0.
 
 
+class SignalTransmitter(Transmitter):
+    """Silent transmitter mock."""
+
+    __num_samples: int
+
+    def __init__(self,
+                 num_samples: int) -> None:
+
+        self.__num_samples = num_samples
+
+    @property
+    def sampling_rate(self) -> float:
+        return self.device.sampling_rate
+
+    def transmit(self,
+                 signal: Signal) -> None:
+
+        self.slot.add_transmission(self, signal)
+
+    @property
+    def frame_duration(self) -> float:
+        return self.__num_samples / self.sampling_rate
+
+
+class SignalReceiver(Receiver):
+    """Noise receiver mock."""
+
+    __num_samples: int
+
+    def __init__(self,
+                 num_samples: int) -> None:
+
+        self.__num_samples = num_samples
+
+    @property
+    def sampling_rate(self) -> float:
+        return self.device.sampling_rate
+
+    @property
+    def energy(self) -> float:
+        return 0.
+
+    def receive(self) -> Signal:
+        return self.signal
+
+    @property
+    def frame_duration(self) -> float:
+        return self.__num_samples / self.sampling_rate
+
+
 class PhysicalDevice(Device):
     """Base representing any device controlling real hardware."""
+
+    __calibration_delay: float
 
     def __init__(self, *args, **kwargs) -> None:
         """
@@ -94,6 +147,19 @@ class PhysicalDevice(Device):
 
         # Init base class
         Device.__init__(self, *args, **kwargs)
+
+        self.__calibration_delay = 0.
+
+    @property
+    def calibration_delay(self) -> float:
+        """Delay compensation of the device calibration.
+
+        Returns:
+            float:
+                Delay compensation in seconds.
+        """
+
+        return self.__calibration_delay
 
     @abstractmethod
     def trigger(self) -> None:
@@ -110,6 +176,10 @@ class PhysicalDevice(Device):
 
         # Signal is now a baseband-signal
         signal.carrier_frequency = 0.
+
+        # Compensate for the calibration
+        delay_compensation = int(self.__calibration_delay * signal.sampling_rate)
+        signal.samples = np.roll(signal.samples, delay_compensation, axis=-1)
 
         for receiver in self.receivers:
 
@@ -156,3 +226,69 @@ class PhysicalDevice(Device):
         noise_power, = power_receiver.receive()
 
         return noise_power
+
+    def calibrate(self,
+                  max_delay: float,
+                  calibration_file: str) -> None:
+        """Calibrate the hardware.
+
+        Currently the calibration will only compensate possible time-delays.
+        Ideally, the transmit and receive channels of the device should be connected by a patch cable.
+        WARNING: An attenuator element may be required! Be careful!!!!
+
+        Args:
+
+            max_delay (float):
+                The maximum expected delay which the calibration should compensate for in seconds.
+
+            calibration_file (str):
+                Location of the calibration dump within the filesystem.
+        """
+
+        # Clear transmit caches
+        self.transmitters.clear_cache()
+
+        num_samples = int(2 * max_delay * self.sampling_rate)
+
+        # Register operators
+        calibration_transmitter = SignalTransmitter(num_samples)
+        self.transmitters.add(calibration_transmitter)
+
+        calibration_receiver = SignalReceiver(num_samples)
+        self.receivers.add(calibration_receiver)
+
+        dirac_index = int(max_delay * self.sampling_rate)
+        waveform = np.zeros((self.num_antennas, num_samples), dtype=complex)
+        waveform[:, dirac_index] = 1.
+        calibration_signal = Signal(waveform, self.sampling_rate, self.carrier_frequency)
+
+        calibration_transmitter.transmit(calibration_signal)
+        self.trigger()
+        propagated_signal = calibration_receiver.receive()
+
+        # Un-register operators
+        self.transmitters.remove(calibration_transmitter)
+        self.receivers.remove(calibration_receiver)
+
+        propagated_dirac_index = np.argmax(abs(propagated_signal.samples[0, :]))
+
+        calibration_delay = (dirac_index - propagated_dirac_index) / propagated_signal.sampling_rate
+
+        # Dump calibration to pickle savefile
+        with open(calibration_file, 'wb') as file_stream:
+            dump(calibration_delay, file_stream)
+
+        # Save register calibration delay internally
+        self.__calibration_delay = calibration_delay
+
+    def load_calibration(self,
+                         calibration_file: str) -> None:
+        """Recall a dumped calibration from the filesystem.
+
+        Args:
+            calibration_file (str):
+                Location of the calibration dump.
+        """
+
+        with open(calibration_file, 'rb') as file_stream:
+            self.__calibration_delay = load(file_stream)
