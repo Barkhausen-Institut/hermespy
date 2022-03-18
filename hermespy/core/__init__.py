1a6ecc8c48c2ea3bf2144687f1dfef334b649ca9
Jan Adler <jan.adler@barkhauseninstitut.org>
2022-03-18 14:43:39 +0100

    Integration tests renamed

--- hermespy/core/__init__.py
+++ hermespy/core/__init__.py
@@ -7,10 +7,10 @@ from .factory import Factory
 
 
 __author__ = "André Noll Barreto"
-__copyright__ = "Copyright 2021, Barkhausen Institut gGmbH"
+__copyright__ = "Copyright 2022, Barkhausen Institut gGmbH"
 __credits__ = ["André Barreto", "Jan Adler"]
 __license__ = "AGPLv3"
-__version__ = "0.2.3"
+__version__ = "0.2.7"
 __maintainer__ = "André Noll Barreto"
 __email__ = "andre.nollbarreto@barkhauseninstitut.org"
 __status__ = "Prototype"
