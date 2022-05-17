5b089870d723dcd1835028a5e050240ecd87b214
Jan Adler <jan.adler@barkhauseninstitut.org>
2022-05-17 10:59:13 +0000

    Test CDL Spatial Properties

--- hermespy/channel/cluster_delay_lines.py
+++ hermespy/channel/cluster_delay_lines.py
@@ -884,7 +884,6 @@ class ClusterDelayLineBase(Channel):
             resampling_matrix = delay_resampling_matrix(sampling_rate, 1, delay, num_delay_samples).flatten()
             impulse_response += np.multiply.outer(coefficients, resampling_matrix)
 
-        print(cluster_delays)
         return impulse_response
 
     @property
