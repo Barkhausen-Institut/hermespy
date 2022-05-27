a0c8c18be02dfd1db591acb00b4e3b5235b711d5
Jan Adler <jan.adler@barkhauseninstitut.org>
2022-05-27 14:53:29 +0200

    Beamforming module unit test skeleton

--- tests/unit_tests/radar/test_fmcw.py
+++ tests/unit_tests/radar/test_fmcw.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from unittest import TestCase
-from unittest.mock import Mock
 
-import numpy as np
-from numpy.testing import assert_array_equal
-from matplotlib.figure import Figure
-
-from hermespy.core import Signal
 from hermespy.radar import FMCW
 
 __author__ = "Jan Adler"
