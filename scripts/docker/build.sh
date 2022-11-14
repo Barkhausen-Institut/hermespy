377f88eccb4f7af4214316a851f6eaee2d86a1e2
Jan Adler <jan.adler@barkhauseninstitut.org>
2022-11-14 14:17:04 +0000

    Introduce InnoSetup Routines

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -7,7 +7,7 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # Force copy only required setup data
 rm -f $SCRIPTPATH/setup.py && rm -f $SCRIPTPATH/README.md
-cp $SCRIPTPATH/../setup.py $SCRIPTPATH && cp $SCRIPTPATH/../README.md $SCRIPTPATH
+cp $SCRIPTPATH/../../setup.py $SCRIPTPATH && cp $SCRIPTPATH/../../README.md $SCRIPTPATH
 
 # Build the docker container
 docker build -t $registry:python-39-linux -f $SCRIPTPATH/linux-build-env $SCRIPTPATH
