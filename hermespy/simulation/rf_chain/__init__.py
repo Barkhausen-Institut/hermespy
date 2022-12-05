001ac181b867902ce0cd5187565e33ff273c7566
Jan Adler <jan.adler@barkhauseninstitut.org>
2022-12-05 14:48:50 +0100

    Removed power law phase noise implementation

--- hermespy/simulation/rf_chain/__init__.py
+++ hermespy/simulation/rf_chain/__init__.py
@@ -1,6 +1,6 @@
 from .rf_chain import RfChain
 from .power_amplifier import PowerAmplifier, SalehPowerAmplifier, RappPowerAmplifier, ClippingPowerAmplifier, CustomPowerAmplifier
-from .phase_noise import PhaseNoise, NoPhaseNoise, PowerLawPhaseNoise
+from .phase_noise import PhaseNoise, NoPhaseNoise
 
 __author__ = "Tobias Kronauer"
 __copyright__ = "Copyright 2021, Barkhausen Institut gGmbH"
@@ -12,4 +12,4 @@ __email__ = "jan.adler@barkhauseninstitut.org"
 __status__ = "Prototype"
 
 
-__all__ = ["RfChain", "PowerAmplifier", "SalehPowerAmplifier", "RappPowerAmplifier", "ClippingPowerAmplifier", "CustomPowerAmplifier", "PhaseNoise", "NoPhaseNoise", "PowerLawPhaseNoise"]
+__all__ = ["RfChain", "PowerAmplifier", "SalehPowerAmplifier", "RappPowerAmplifier", "ClippingPowerAmplifier", "CustomPowerAmplifier", "PhaseNoise", "NoPhaseNoise"]
